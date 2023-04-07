374ef9c7117dbe13a53ec2c8609f7a8cc14c7e93
kalip <kalipedavid@gmail.com>
2023-04-07 20:04:06 +0000

    changement

--- monportfolio/src/components/Experiences.jsx
+++ monportfolio/src/components/Experiences.jsx
@@ -6,9 +6,12 @@ import REACT from '../assets/react.png'
 import KOTLIN from '../assets/kotlin.png'
 import FLUTTER from '../assets/flutter.png'
 import BOOTSTRAP from '../assets/bootstrap.png'
+import DJANGO from '../assets/django.png'
 import FLASK from '../assets/flask.png'
 import SQL from '../assets/sql.png'
-
+import Firebase from '../assets/firebase.png'
+import Java from '../assets/java.png'
+import Git from '../assets/git.png'
 
 
 
@@ -16,12 +19,11 @@ const Experiences = () => {
   return (
     <div name='experiences' className='w-full h-screen bg-[#0a192f] text-gray-300 py-4'>
         {/* CONTAINER */}
-        <div className='max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full'>
+        <div className='max-w-[1000px] mx-auto p-8 flex flex-col justify-center w-full h-full'>
             <div>
                 <p className='text-5xl font-bold inline border-b-4 border-red-400 '>Experiences</p>
                 <p className='py-4 text-2xl'>// These are the technologies I've worked with</p>
             </div>
-
             <div className='w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8'>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto my-4' src={HTML} alt="HTML icon" />
@@ -40,11 +42,23 @@ const Experiences = () => {
                     <p className='my-4'>REACT</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto my-4' src={SQL} alt="SQL icon" />
+                    <img className='w-28 mx-auto my-4' src={SQL} alt="SQL icon" />
                     <p className='my-4'>SQL</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
-                    <img className='w-20 mx-auto my-4' src={FLASK} alt="FLASK icon" />
+                    <img className='w-20 mx-auto my-4' src={Firebase} alt="SQL icon" />
+                    <p className='my-4'>FIREBASE</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-30 mx-auto my-4' src={Java} alt="SQL icon" />
+                    <p className='my-4'>JAVASCRIPT</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-30 mx-auto my-4' src={DJANGO} alt="FLASK icon" />
+                    <p className='my-4'>DJANGO</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-30 mx-auto my-4' src={FLASK} alt="FLASK icon" />
                     <p className='my-4'>FLASK</p>
                 </div>
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
@@ -54,6 +68,10 @@ const Experiences = () => {
                 <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
                     <img className='w-20 mx-auto my-4' src={KOTLIN} alt="KOTLIN icon" />
                     <p className='my-4'>KOTLIN</p>
+                </div>
+                <div className='shadow-md shadow-[#040c16] hover:scale-110 duration-500'>
+                    <img className='w-30 mx-auto my-4' src={Git} alt="SQL icon" />
+                    <p className='my-4'>GITHUB</p>
                 </div>
             </div>
         </div>
